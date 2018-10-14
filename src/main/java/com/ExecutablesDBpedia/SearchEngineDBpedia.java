9eb130736bcbb6de12408a91b87ebce5f3af777c
sylvia.wang <sylvia.wang@ringcentral.com>
2018-10-14 22:33:17 +0800

    refactor TagUtil

--- src/main/java/com/ExecutablesDBpedia/SearchEngineDBpedia.java
+++ src/main/java/com/ExecutablesDBpedia/SearchEngineDBpedia.java
@@ -13,38 +13,38 @@ public final class SearchEngineDBpedia {
 
 
     public static void main(String[] args) {
-        //EntityToSW entityToSW = new EntityToSW();
+        EntityToSW entityToSW = new EntityToSW();
 
 
-        //List<List<String>> tris = entityToSW.getTriples(0, 50);
-        //ESEngineSWDBpedia esEngine = new ESEngineSWDBpedia();
+        List<List<String>> tris = entityToSW.getTriples(50, 1000);
+        ESEngineSWDBpedia esEngine = new ESEngineSWDBpedia();
 
-        String s = "<head> test1 </head> test2 <tail> test3 </tail> <head> test4 </head> ddd5 <tail> test6 </tail> tt <head> test7 </head>";
-        TagUtil tagUtil = new TagUtil();
-        tagUtil.mergeTagsNearest(s);
+//        String s = "<head> test1 </head> test2 <tail> test3 </tail> <head> test4 </head> ddd5 <tail> test6 </tail> tt <head> test7 </head>";
+//        TagUtil tagUtil = new TagUtil();
+//        tagUtil.mergeTagsNearest(s);
 
-//        int i = 0;
-//        try {
-//            for (int j = 0; j < tris.size(); j++) {
-//                List<String> tri = tris.get(j);
-//                List<String> trimedTri = trimEntity(tri);
-//                esEngine.searchAndTagAndSave(trimedTri);
-//
-//                try {
-//                    Thread.sleep(100);
-//                } catch (Exception e) {
-//                    System.out.println("interrupted");
-//                }
-//
-//                i = j;
-//                System.out.println("last triple index: " + String.valueOf(i));
-//            }
-//        } catch (Exception e) {
-//            System.out.println(e);
-//            throw e;
-//        } finally {
-//            System.out.println("last triple index: " + String.valueOf(i));
-//        }
+        int i = 0;
+        try {
+            for (int j = 0; j < tris.size(); j++) {
+                List<String> tri = tris.get(j);
+                List<String> trimedTri = trimEntity(tri);
+                esEngine.searchAndTagAndSave(trimedTri);
+
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                    System.out.println("interrupted");
+                }
+
+                i = j;
+                System.out.println("last triple index: " + String.valueOf(i));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            throw e;
+        } finally {
+            System.out.println("last triple index: " + String.valueOf(i));
+        }
     }
 
     private static List<String> trimEntity(List<String> uris) {
